98c157ef8521e0af01020a7bc95c00bce7cee30f
Ayodhyankit Paul <3x3paul@gmail.com>
2017-09-12 02:01:10 +0530

    Update Simple_decorator.py

--- Simple_decorator.py
+++ Simple_decorator.py
@@ -11,3 +11,20 @@ def New_func(x):
 
 New_func=simple(New_func)
 New_func(12)
+
+
+#Instead of calling New_func=simple(New_func) we could use  @simple  which is called syntactic sugar
+
+def simple(func):
+    def wrapper(xx):
+        print("before the decorator")
+        func(xx)
+        print("after the decorator ")
+    return wrapper
+
+
+@simple
+def New_func(x):
+    print("calling this new function")
+
+New_func(12)
